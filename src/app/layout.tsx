690e92fe8302c56d03fb424ceec57b95ccb2a3dc
Wayter Paulo <wayter.paulo.95@gmail.com>
2026-03-15 21:12:52 -0300

    feat: add tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "../styles/globals.css";
 
 const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const jakarta = Plus_Jakarta_Sans({
   variable: "--font-jakarta",
@@ -58,6 +60,29 @@ export default function RootLayout({
         className={`${jakarta.variable} overflow-x-hidden font-sans antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager */}
+        {GTM_ID && (
+          <>
+            <Script id="gtm" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+            </Script>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+                title="Google Tag Manager"
+              />
+            </noscript>
+          </>
+        )}
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         {META_PIXEL_ID && (
           <>
@@ -91,6 +116,26 @@ export default function RootLayout({
           </>
         )}
         {/* End Meta Pixel Code */}
+
+        {/* Google tag (gtag.js) */}
+        {GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        )}
+        {/* End Google tag */}
+
         {children}
       </body>
     </html>
